585299ac610b6e8cb708557a0010022e714bf77d
João Paulo <jpaulope@indracompany.com>
2021-11-11 12:29:38 -0300

    feat: simple pointers and array example

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int *numbers, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d) %d\n", i + 1, numbers[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
-    int number = 0;
+    const int length = 5;
 
-    scanf("%d", &number);
+    int v[5] = {1, 2, 3, 4, 5};
 
-    printf("hey, you typed the number %d!", number);
+    int *p = v;
+
+    printArray(v, length);
+    printArray(p, length);
 
     return 0;
 }
